2fbc18806a0e80f521427601606584819fd5ddf7
mandeep singh bhatia <mandeep.singh.bhatia@gmail.com>
2016-07-08 18:36:30 +0800

    bug fix

--- src/time-map.scm
+++ src/time-map.scm
@@ -1,12 +1,13 @@
-(use-modules (opencog) (opencog atom-types) (opencog-exec) (opencog ato pointmem)); needed for mapsi
+(use-modules ((rnrs) :version(6)))
+(use-modules (opencog) (opencog atom-types) (opencog ato pointmem)); needed for mapsi
 (use-modules (opencog python))
 ;;initialize octomap with 15hz, 10 second or 150 frames buffer ; 1 cm spatial resolution
-(create-map "faces" 0.01 (/ 1000 15) 150) (step-time-unit "faces")(auto-step-time-on "faces")
+(create-map "faces" 0.01 (div 1000 15) 150) (step-time-unit "faces")(auto-step-time-on "faces")
 ;;scm code
 (define (look-at-face face-id-node)
-		(let* ((loc-atom (get-past-locs-ato "faces" face-id-node (/ 1000 15)))
+		(let* ((loc-atom (get-past-locs-ato "faces" face-id-node (div 1000 15)))
 			(fnc "evl.look_at_point("))
-			(if (equal? cog-undefined-handle (cog-handle loc-atom)) #f 
+			(if (equal? (cog-atom (cog-undefined-handle)) loc-atom) #f 
 				(let* ((loc-link (car (cog-outgoing-set loc-atom)))
 					(xx (number->string (loc-link-x loc-link)))
 					(yy (number->string (loc-link-y loc-link)))
