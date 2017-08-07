81b852f4b4a87caf651672c9fc52470cdeac10da
Amen Belayneh <amenbelayneh@gmail.com>
2017-08-07 16:24:05 +0800

    update setup script

--- scripts/setup.bash
+++ scripts/setup.bash
@@ -2,8 +2,13 @@
 #
 # Script for setting up HEAD for building rbs
 
+# TODO move to octool.
+GIT_REPO="$(dirname $(readlink -f ${BASH_SOURCE[0]}))/.."
+
 curl https://raw.githubusercontent.com/hansonrobotics/hrtool/master/get_hr.bash|bash
 
 hr install head-deps
 echo yes | hr init
 hr update head
+hr cmd get_head
+ln -s "$GIT_REPO" ~/hansonrobotics/HEAD/src/ros-behavior-scripting
