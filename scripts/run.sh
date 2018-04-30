734d67528f3268430fe770df4835c5ab4131deff
elggem <trilly@gmail.com>
2018-04-30 12:56:53 +0800

    cleanup and update of scripts

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 #
 # Script for running opencog with HEAD.
+#
+#
 
 set -e
 _session_name="opencog"
@@ -14,9 +16,7 @@ HR_WS="$(hr env | grep HR_WORKSPACE | cut -d = -f 2)"
 # the PYTHONPATH
 PYTHON_PATH="${PYTHONPATH}:/usr/local/lib/python2.7/dist-packages"
 
-
-# NOTE: Since the HEAD services may fail randomly, start opencog
-# in separate tmux session.
+# start opencog processes in tmux session.
 start_opencog_tmux_session()
 {
   echo "Start opencog services in a new background tmux session"
@@ -27,23 +27,23 @@ start_opencog_tmux_session()
     $SHELL"
 
   # Start the cogserver
-  tmux new-window -t "$_session_name:" -n "ghost" \
+  tmux new-window -t "$_session_name:" -n "cogserver" \
     "cd $HR_WS/OpenCog/ros-behavior-scripting/scripts &&
-    guile -l config.scm;
-    $SHELL"
+    cogserver ./cogserver.conf"
 
   # Start passing sensory inputs to the cogserver
-  tmux new-window -t "$_session_name:" -n "rbs" \
-    "export PYTHONPATH=$PYTHON_PATH &&
-    cd $HR_WS/OpenCog/ros-behavior-scripting/sensors &&
-    python main.py ;
-    $SHELL"
+  #tmux new-window -t "$_session_name:" -n "rbs" \
+  #  "export PYTHONPATH=$PYTHON_PATH &&
+  #  cd $HR_WS/OpenCog/ros-behavior-scripting/sensors &&
+  #  python main.py ;
+  #  $SHELL"
+
+  # Start the cogserver
+  tmux new-window -t "$_session_name:" -n "telnet-cogserver" \
+    "rlwrap telnet localhost 17001"
 
   echo "Finished starting opencog services in a new background tmux session"
 }
 
 # Start opencog tmux session
 tmux has-session -t "$_session_name" || start_opencog_tmux_session
-
-# Start HEAD tmux session
-tmux has-session -t "sophia10" || hr run
